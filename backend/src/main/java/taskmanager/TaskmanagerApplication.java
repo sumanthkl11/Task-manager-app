a2b4b7c37ad14b4851446d10ae58decf4d75f6dc
Sumanth K L <sumanthkl1428@gmail.com>
2026-05-13 00:13:53 +0530

    Initial Spring Boot Setup

---
+++ backend/src/main/java/taskmanager/TaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerApplication.class, args);
+	}
+
+}
